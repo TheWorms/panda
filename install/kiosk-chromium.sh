c705d5af5053d6db1df41b9b5b024624959591f9
The Worm's <theworms@theworms.fr>
2026-07-18 00:02:28 +0200

    retrait kiosk-chromium.sh obsolète (remplacé par panda-kiosk dans install.sh)

--- install/kiosk-chromium.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Lance Chromium en mode kiosk plein écran sur l'interface.
-# À utiliser quand un bureau/écran est présent (Panda, ou VM 703 avec X).
-# Sur la 703 en test, tu peux simplement ouvrir l'URL dans un navigateur
-# et redimensionner la fenêtre à 1024x600.
-URL="${1:-http://panda.local:8090}"
-exec chromium --kiosk --incognito --noerrdialogs --disable-infobars \
-  --window-size=1024,600 --window-position=0,0 \
-  --check-for-update-interval=31536000 "${URL}"
